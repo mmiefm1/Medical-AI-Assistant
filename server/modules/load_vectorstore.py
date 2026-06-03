a5aba59f646173413967e7b8090860e6b49f2a51
Pratham Pal <mmiefm1@gmail.com>
2026-06-04 04:08:05 +0530

    fix startup crash for Render deployment

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -2,54 +2,41 @@ import os
 import time
 from pathlib import Path
 from dotenv import load_dotenv
-from langchain_community.embeddings import HuggingFaceEmbeddings
 from tqdm.auto import tqdm
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from google.genai import types
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
-GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
-PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
-PINECONE_ENV="us-east-1"
-PINECONE_INDEX_NAME="medicalindex"
+PINECONE_ENV = "us-east-1"
+PINECONE_INDEX_NAME = "medicalindex"
 
-os.environ["GOOGLE_API_KEY"]=GOOGLE_API_KEY
-
-UPLOAD_DIR="./uploaded_docs"
-os.makedirs(UPLOAD_DIR,exist_ok=True)
-
-
-# initialize pinecone instance
-pc=Pinecone(api_key=PINECONE_API_KEY)
-spec=ServerlessSpec(cloud="aws",region=PINECONE_ENV)
-existing_indexes=[i["name"] for i in pc.list_indexes()]
-
-
-if PINECONE_INDEX_NAME not in existing_indexes:
-    pc.create_index(
-        name=PINECONE_INDEX_NAME,
-        dimension=384,
-        metric="dotproduct",
-        spec=spec
-    )
-    while not pc.describe_index(PINECONE_INDEX_NAME).status["ready"]:
-        time.sleep(1)
-
-
-index=pc.Index(PINECONE_INDEX_NAME)
-
-# load,split,embed and upsert pdf docs content
+UPLOAD_DIR = "./uploaded_docs"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 def load_vectorstore(uploaded_files):
-    embed_model = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
-    )
-    file_paths = []
+    # Initialize Pinecone inside function not at startup
+    pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+    spec = ServerlessSpec(cloud="aws", region=PINECONE_ENV)
+    existing_indexes = [i["name"] for i in pc.list_indexes()]
 
+    if PINECONE_INDEX_NAME not in existing_indexes:
+        pc.create_index(
+            name=PINECONE_INDEX_NAME,
+            dimension=384,
+            metric="dotproduct",
+            spec=spec
+        )
+        while not pc.describe_index(PINECONE_INDEX_NAME).status["ready"]:
+            time.sleep(1)
+
+    index = pc.Index(PINECONE_INDEX_NAME)
+
+    embed_model = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+
+    file_paths = []
     for file in uploaded_files:
         save_path = Path(UPLOAD_DIR) / file.filename
         with open(save_path, "wb") as f:
